b307278fb34141d1e5d0506b95e99408bce708cf
Arshvir <avarshvir@gmail.com>
2024-07-22 22:26:59 +0530

    commit

---
+++ c++/1_Arrays/array_on_the_basis_of_dimensions/1_arrays_1d/11_array_linear_search.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout << "Enter the size of array : ";
+    cin >> n;
+
+    int als[n];
+    cout << "Enter array elements : " << endl;
+    for (int i = 0; i < n; i++){
+        cin >> als[i];
+    } 
+    
+    int element;
+    cout << "Enter element to search : ";
+    cin >> element;
+
+    bool found = false;
+
+    for(int i = 0; i < n; i++){
+        if(als[i] == element){
+            cout << "Element is found at index [" << i << "]" << "and position is : "<< i+1 << endl;
+            found = true;
+            break;
+        }
+    }
+    if(!found){
+            cout << "Element not found " << endl;
+        }
+
+
+}
