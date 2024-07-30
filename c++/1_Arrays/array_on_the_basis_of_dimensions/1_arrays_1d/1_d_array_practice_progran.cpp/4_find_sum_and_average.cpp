656809a95da7e13cf0fa14c61e2de35c90fb40d7
Arshvir <avarshvir@gmail.com>
2024-07-30 19:17:44 +0530

    commit

--- c++/1_Arrays/array_on_the_basis_of_dimensions/1_arrays_1d/1_d_array_practice_progran.cpp/4_find_sum_and_average.cpp
+++ c++/1_Arrays/array_on_the_basis_of_dimensions/1_arrays_1d/1_d_array_practice_progran.cpp/4_find_sum_and_average.cpp
@@ -1,14 +1,14 @@
-#include<iostream>
-using namespace std;
-int main(){
-    int arr[5] = {1,2,3,4,5};
-    int sum = 0;
-    float avg;
-    int size = sizeof(arr)/sizeof(arr[0]);
-    for(int i = 0; i < 5; i++){
-        sum = sum + arr[i];
+    #include<iostream>
+    using namespace std;
+    int main(){
+        int arr[6] = {1,2,3,4,5,6};
+        int sum = 0;
+        float avg;
+        int size = sizeof(arr)/sizeof(arr[0]);
+        for(int i = 0; i < 6; i++){
+            sum = sum + arr[i];
+        }
+        cout << "sum : " << sum << endl;
+        avg = float(sum)/size;
+        cout << "average : " << avg << endl;
     }
-    cout << "sum : " << sum << endl;
-    avg = sum/size;
-    cout << "average : " << avg << endl;
-}
