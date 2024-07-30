cc7cdccfd790d6b638ea18c8171b104549cd3945
Arshvir <avarshvir@gmail.com>
2024-07-30 21:51:52 +0530

    commit

---
+++ c++/1_Arrays/STL_array/1_stl_array.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<array>
+using namespace std;
+int main(){
+    array<int,5> arr = {1,2,3,4,5};
+    int size = arr.size();
+    for(int i = 0; i< size; i++){
+            cout << arr[i] << " ";
+    }
+    cout << endl;
+    cout << arr[4] << endl;
+
+    cout << "using .at function : " << arr.at(3) << endl;
+    cout << "Empty or Not : " << arr.empty() << endl;
+
+    cout << "First Element : " << arr.front() << endl;
+    cout << "Last Element : "<< arr.back() << endl;
+    //cout << "data : " << arr.data() << endl;
+
+}
