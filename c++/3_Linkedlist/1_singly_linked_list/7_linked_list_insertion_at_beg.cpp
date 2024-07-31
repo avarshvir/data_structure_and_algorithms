1463cbf736de3fd4daa8141ad352e848714dd8df
Arshvir <avarshvir@gmail.com>
2024-07-31 12:58:01 +0530

    commit

--- c++/3_Linkedlist/1_singly_linked_list/7_linked_list_insertion_at_beg.cpp
+++ c++/3_Linkedlist/1_singly_linked_list/7_linked_list_insertion_at_beg.cpp
@@ -3,45 +3,45 @@ using namespace std;
 class Node{
     public:
     int data;
-    Node *next;
+    Node * next;
 };
 
-void traversal(Node *ptr){
+void traverse(Node *ptr){
     while(ptr!= nullptr){
         cout << ptr->data << " ";
         ptr = ptr->next;
     }
+    cout << endl;
 }
 
-Node* insertionAtBeg(Node *head, int data){
-    Node *new_node = new Node();
-    new_node->next = head;
+Node* insertAtBeg(Node *&head, int data){      //& because I want dont want copy I need original, It is our choice we can also run without &
+    Node* new_node = new Node();
     new_node->data = data;
+    new_node->next = head;
     return new_node;
-
 }
 
 int main(){
     Node *head = new Node();
     Node *second = new Node();
     Node *third = new Node();
-    
+
     head->data = 11;
     head->next = second;
 
     second->data = 22;
     second->next = third;
-    
+
     third->data = 33;
     third->next = nullptr;
-    
-    cout << "Before Insertion : ";
-    traversal(head);
 
-    cout << endl;
+    cout << "Before Insertion : ";
+    traverse(head);
 
     cout << "After Insertion : ";
-    head = insertionAtBeg(head,1);
-    traversal(head);
+    head = insertAtBeg(head, 1);
+    traverse(head);
 
+    
+    
 }
