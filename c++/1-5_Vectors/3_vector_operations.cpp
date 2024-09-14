bd15817bdfb50e86f14edf5f230e07c9e9679492
Arshvir <avarshvir@gmail.com>
2024-09-14 21:18:14 +0530

    commit

---
+++ c++/1-5_Vectors/3_vector_operations.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+    vector<int> vec = {1,2,3,4,5};
+    cout << "size : " << vec.size() << endl;
+    //int size = sizeof(vec) / sizeof(vec[0]);
+    //cout << "manually size" << size << endl;
+}
