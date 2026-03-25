457ddac3ca18f49601744eb57ac2c1f924076529
avarshvir <avarshvir@gmail.com>
2026-03-25 15:41:23 +0530

    Creating UMS

---
+++ c++/STL/1_Containers/1_4_Unordered_Associative_Containers/1_4_3_unordered_multiset/1_ums.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<unordered_set>
+
+using namespace std;
+int main(){
+
+    // Create an empty unordered multiset
+    unordered_multiset<int> ums1;
+
+    // Create and initialize elements
+    // using initialzer list
+    unordered_multiset<int> ums2 =
+                          {5, 3, 4, 1, 1};
+    
+    for(auto x : ums2)
+        cout << x << " ";
+
+    return 0;
+
+}
